5c3ed2614d0ba2348968787196577870695cbc23
ErickYar <erickyb1985@gmail.com>
2023-12-03 06:17:42 -0500

    creando variable de la calculadora y también el Resultado en otra página

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-  path('',views.index,name='app/index.html')
+  path('',views.index,name='index'),
+  path('procesar/',views.procesar,name='procesar'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 def index(request):
     return render(request,'app/index.html')
 
+def procesar(request):
+    #recuperar datos de la página index
+    n1=float(request.GET['num1'])
+    n2=float(request.GET['num2'])
+    ope=request.GET.get('operador')
+    res=0
+    match ope:
+        case '+':
+            res= n1+n2
+        case '-':
+            res= n1-n2
+        case 'X':
+            res= n1*n2
+        case '/':
+            res= n1/n2
+        case _:
+            res= 0
+    return render(request,'app/resultado.html',{'result':res}) 
+
+
